83fb0676c2680cc3a8def841885777ccc8360235
Khadija Riya <khadejariya49@gmail.com>
2020-07-20 11:14:42 +0300

    deposit part completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
-//for login part
+//login button event create
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click" , function(){
- const loginArea = document.getElementById("login-area");
+    const loginArea = document.getElementById("login-area");
     loginArea.style.display = "none";
     const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+})
+
+//deposit button event create
+const depositBtn = document.getElementById("deposit-button");
+depositBtn.addEventListener("click" , function(){
+    const depositAmount = document.getElementById("depositAmount").value;
+    const depositAmountNumber = parseFloat(depositAmount);
+
+    const currentDeposit = document.getElementById("currentDeposit").innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositAmountNumber + currentDepositNumber;
+    document.getElementById("currentDeposit").innerText = totalDeposit;
+    
+    const currentBalance = document.getElementById("currentBalance").innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = depositAmountNumber + currentBalanceNumber;
+    document.getElementById("currentBalance").innerText = totalBalance;
+
+    document.getElementById("depositAmount").value = "";
 })
